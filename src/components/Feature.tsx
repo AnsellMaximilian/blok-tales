9e8862ec60eb7a74ab31d6f8622f264dde6005d0
Ansell Maximilian <ansellmaximilian@gmail.com>
2025-06-26 10:55:35 +0700

    remove unused components

--- src/components/Feature.tsx
+++
@@ -1,9 +0,0 @@
-import { storyblokEditable, type SbBlokData } from "@storyblok/react";
-
-const Feature = ({ blok }: { blok: SbBlokData }) => (
-  <div {...storyblokEditable(blok)} className="bg-red-400">
-    {blok.name as string}
-  </div>
-);
-
-export default Feature;
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { apiPlugin, storyblokInit } from "@storyblok/react";
-import Page from "./components/Page.tsx";
-import Teaser from "./components/Teaser.tsx";
-import Feature from "./components/Feature.tsx";
-import Grid from "./components/Grid.tsx";
+
 import Scene from "./components/tales/Scene.tsx";
 import Narration from "./components/tales/Narration.tsx";
 import Dialog from "./components/tales/Dialog.tsx";
@@ -19,11 +16,6 @@ storyblokInit({
   accessToken: "jdeMxRmerezgRgrCm0SMLgtt",
   use: [apiPlugin],
   components: {
-    page: Page,
-    teaser: Teaser,
-    feature: Feature,
-    grid: Grid,
-
     //tales
     scene: Scene,
     narration: Narration,
